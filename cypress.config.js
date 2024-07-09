b950f8c20d1c364d4dfcfda4f9ef0d92294f6009
TaiseQa <taisealmeida713@gmail.com>
2024-07-09 12:41:26 -0300

    teste mesclagem de relatorio

--- cypress.config.js
+++ cypress.config.js
@@ -9,29 +9,35 @@ module.exports = defineConfig({
     specPattern: "cypress/features/execution/*.feature",
     supportFile: "cypress/support/e2e.js",
     experimentalInteractiveRunEvents: true,
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: false,
+      json: true,
+    },
     setupNodeEvents(on, config) {
 
       on('file:preprocessor', cucumber())
 
-      // on('before:spec', async () => {
-      //   const screenShotDirectory = path.join(__dirname, 'cypress/screenshots');
-      //   console.log(`INFO......: Verificando a existência de ${screenShotDirectory}`);
+      on('before:spec', async () => {
+        const screenShotDirectory = path.join(__dirname, 'cypress/screenshots');
+        console.log(`INFO......: Verificando a existência de ${screenShotDirectory}`);
 
-      //   try {
-      //     const exists = await fs.pathExists(screenShotDirectory);
-      //     console.log(`INFO......: Existência do diretório: ${exists}`);
-
-      //     if (exists) {
-      //       console.log(`INFO......: Tentando deletar o diretório...`);
-      //       await fs.remove(screenShotDirectory);
-      //       console.log(`INFO......: Diretório deletado com sucesso!`);
-      //     } else {
-      //       console.log(`INFO......: Diretório não existe.`);
-      //     }
-      //   } catch (err) {
-      //     console.error(`ERROR......: Falha ao deletar o diretório de screenshots.`, err);
-      //   }
-      // });
+        try {
+          const exists = await fs.pathExists(screenShotDirectory);
+          console.log(`INFO......: Existência do diretório: ${exists}`);
+          if (exists) {
+            console.log(`INFO......: Tentando deletar o diretório...`);
+            await fs.remove(screenShotDirectory);
+            console.log(`INFO......: Diretório deletado com sucesso!`);
+          } else {
+            console.log(`INFO......: Diretório não existe.`);
+          }
+        } catch (err) {
+          console.error(`ERROR......: Falha ao deletar o diretório de screenshots.`, err);
+        }
+      });
 
       cypressSplit(on, config)
       // IMPORTANT: return the config object
